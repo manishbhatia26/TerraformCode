0986f6d9421da5547d5accf6b10d6a184a98c7fb
manishbhatia26 <manishbhatia26@gmail.com>
2026-04-15 13:20:29 +0530

    Update provider.tf

--- ForeachModule/modules/rg/provider.tf
+++ ForeachModule/modules/rg/provider.tf
@@ -1,6 +1,8 @@
 provider "azuerm" {
-features{}
- subscription_id = var.subscription_id
+features{
+required_version = "1.14.8
+}
+subscription_id = var.subscription_id
 tenant_id       = var.tenant_id
 client_id       = var.client_id
 client_secret   = var.client_secret
