16dee5a4fbf675ca06fb7de45d5377637b24d153
manishbhatia26 <manishbhatia26@gmail.com>
2026-04-15 13:22:54 +0530

    added terraform provider version

--- ForeachModule/modules/rg/provider.tf
+++ ForeachModule/modules/rg/provider.tf
@@ -1,10 +1,21 @@
 provider "azuerm" {
 features{
-required_version = "1.14.8
+
 }
+
+
 subscription_id = var.subscription_id
 tenant_id       = var.tenant_id
 client_id       = var.client_id
 client_secret   = var.client_secret
 
+}
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "1.14.8"
+    }
+  }
 }
